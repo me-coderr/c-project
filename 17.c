fbd014e98c2cfa7de06b38970ceee44d9c741e64
me-coderr <me.coderr@gmail.com>
2022-10-09 23:44:39 +0530

    numner 17 has some logical error, wrong output

---
+++ 17.c
@@ -0,0 +1,95 @@
+/*Reverse the list of elements in a circular linked list.*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    struct node *next;
+    int data;
+};
+
+struct node *head=NULL;
+struct node *head_rev=NULL;
+
+void create(int n)
+{
+    struct node *temp, *newnode;
+    for(int i=1;i<=n;i++)
+    {
+        temp=head;
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("\nenter data for node %d\n",i);
+        scanf("%d",&newnode->data);
+        if(head==NULL)
+        {
+            head=newnode;
+        }
+        else
+        {
+            while(temp->next!=head)
+            {
+                temp=temp->next;
+            }
+            temp->next=newnode;
+        }
+        newnode->next=head;
+    }
+    printf("\n");
+}
+
+void display(struct node *headDisplay)
+{
+    struct node *temp=headDisplay->next;
+    printf("%d\t",headDisplay->data);
+    while(temp!=headDisplay)
+    {
+        printf("%d\t",temp->data);
+        temp=temp->next;
+    }
+    printf("::\t%d",temp->data);
+    printf("\n");
+}
+
+void reverse(int n)
+{
+    struct node *temp_rev, *temp;
+    int c=n;
+    while(c--!=0)
+    {
+        temp=head;
+        for(int i=1;i<=c;i++)
+        {
+            temp=temp->next;
+        }
+        if(head_rev==NULL)
+        {
+            head_rev=temp;
+            temp_rev=head_rev;
+        }
+        else
+        {
+            temp_rev->next=temp;
+            temp_rev=temp_rev->next;
+        }
+    }
+    free(temp);
+    temp_rev->next=head_rev;
+    temp_rev=temp_rev->next;
+}
+
+int main()
+{
+    int n;
+    printf("\ncreate a circular list first:\n");
+    printf("enter the number of nodes\n");
+    scanf("%d",&n);
+    create(n);
+    printf("\nthe circular list you entered:\n");
+    display(head);
+    reverse(n);
+    printf("\nthe circular list after being reversed:\n");
+    display(head_rev);
+    return 0;
+}
